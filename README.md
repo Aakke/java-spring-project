996f8acf6659d6840b92d508a05dd68e2bebcd20
Aleksi Tani <aleksi.e.o.tani@student.jyu.fi>
2020-09-15 13:49:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java spring project
+
+## Tools and environment
+- Eclipse 
+- Java 11
+- H2-database
+- Thymeleaf
+- Maven
+
+
+## Author
+Aleksi Tani - aleksitani@gmail.com
---
+++ src/main/java/com/github/aakke/javaspringproject/JavaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.aakke.javaspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringProjectApplication.class, args);
+	}
+
+}
