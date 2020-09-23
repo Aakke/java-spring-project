abef4cb65eed0f3149c8b3b723447ef6d45d0ba8
Aleksi Tani <aleksi.e.o.tani@student.jyu.fi>
2020-09-23 20:38:07 +0300

    Fix endpoint paths for readerController and add GET for all

--- src/main/java/com/github/aakke/javaspringproject/controller/ReaderController.java
+++ src/main/java/com/github/aakke/javaspringproject/controller/ReaderController.java
@@ -1,6 +1,9 @@
 package com.github.aakke.javaspringproject.controller;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.aakke.javaspringproject.model.Reader;
 import com.github.aakke.javaspringproject.service.ReaderService;
 
-@RestController
-@RequestMapping("reader")
+@RestController // Spring Rest Controller.
+@RequestMapping("/api")
 public class ReaderController {
 
 	@Autowired
 	private ReaderService readerService;
 	
-	@GetMapping("/{id}")//, produces = "application/json")
-	public Reader getReader(@PathVariable int id) {
-		return readerService.findById(id);
+	@GetMapping("/reader/{id}")//, produces = "application/json")
+	public ResponseEntity<Reader> getReader(@PathVariable int id) {
+		return ResponseEntity.ok().body(readerService.findById(id));
+	}
+	
+	@GetMapping("/readers")
+	public Collection<Reader> getAllReaders() {
+		return readerService.findAll();
 	}
 }
