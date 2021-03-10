1cc09b6926cec83e620db3b93096ffb9db88f78c
Aleksi Tani <aleksitani@gmail.com>
2021-03-10 20:57:11 +0200

    Add all basic CRUD operations for AuthorService.

--- src/main/java/com/github/aakke/javaspringproject/service/AuthorService.java
+++ src/main/java/com/github/aakke/javaspringproject/service/AuthorService.java
@@ -1,5 +1,42 @@
 package com.github.aakke.javaspringproject.service;
 
+import java.util.List;
+
+import com.github.aakke.javaspringproject.model.Author;
+
 public interface AuthorService {
 
+	/**
+	 * Find author by id.
+	 * @param id author id
+	 * @return author
+	 */
+	Author findById(long id);
+	
+	/**
+	 * Find all authors.
+	 * @return all authors
+	 */
+	List<Author> findAll();
+	
+	/**
+	 * Delete author by id.
+	 * @param id author id
+	 */
+	void deleteById(long id);
+	
+	/**
+	 * Create new author.
+	 * @param newAuthor new author
+	 * @return new author
+	 */
+	Author create(Author newAuthor);
+	
+	/**
+	 * Update author by id.
+	 * @param id author id
+	 * @param updateAuthor updated author
+	 * @return updated author
+	 */
+	Author update(long id, Author updateAuthor);
 }
--- src/main/java/com/github/aakke/javaspringproject/service/AuthorServiceImpl.java
+++ src/main/java/com/github/aakke/javaspringproject/service/AuthorServiceImpl.java
@@ -1,5 +1,59 @@
 package com.github.aakke.javaspringproject.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.aakke.javaspringproject.model.Author;
+import com.github.aakke.javaspringproject.repository.AuthorRepository;
+
+@Service
 public class AuthorServiceImpl implements AuthorService{
 
+	private final AuthorRepository authorRepo;
+	
+	@Autowired
+	public AuthorServiceImpl(AuthorRepository authorRepo) {
+		this.authorRepo = authorRepo;
+	}
+
+	@Override
+	public Author findById(long id) {
+		return authorRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("No author found."));
+	}
+
+	@Override
+	public List<Author> findAll() {
+		var allAuthors = authorRepo.findAll();
+		List<Author> listOfAuthors = new ArrayList<>();
+		allAuthors.forEach(listOfAuthors::add);
+		return listOfAuthors;
+	}
+
+	@Override
+	public void deleteById(long id) {
+		authorRepo.delete(findById(id));
+	}
+
+	@Override
+	public Author create(Author newAuthor) {
+		return authorRepo.save(newAuthor);
+	}
+
+	@Override
+	public Author update(long id, Author updateAuthor) {
+		var author = findById(id);
+		
+		author.setBirthYear(updateAuthor.getBirthYear());
+		updateAuthor.getBooks().forEach(b -> author.addBook(b));
+		author.setName(updateAuthor.getName());
+		
+		return authorRepo.save(author);
+	}
+	
+	
 }
