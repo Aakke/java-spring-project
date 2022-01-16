d10032ca9e03922343480794f98d582b88131f92
Aleksi Tani <aleksitani@gmail.com>
2022-01-16 21:05:28 +0200

    Base impl of repo methods for bookService.

--- src/main/java/com/github/aakke/javaspringproject/service/BookService.java
+++ src/main/java/com/github/aakke/javaspringproject/service/BookService.java
@@ -1,5 +1,42 @@
 package com.github.aakke.javaspringproject.service;
 
+import java.util.List;
+
+import com.github.aakke.javaspringproject.model.Book;
+
 public interface BookService {
 
+	/**
+	 * Find book by id.
+	 * @param id book id
+	 * @return book
+	 */
+	Book findById(long id);
+	
+	/**
+	 * Find all books.
+	 * @return all books
+	 */
+	List<Book> findAll();
+	
+	/**
+	 * Delete book by id.
+	 * @param id book id
+	 */
+	void deleteById(long id);
+	
+	/**
+	 * Create new book.
+	 * @param newBook new book
+	 * @return new book
+	 */
+	Book create(Book newBook);
+	
+	/**
+	 * Update book by id.
+	 * @param id book id
+	 * @param updateBook updated book
+	 * @return updated book
+	 */
+	Book update(long id, Book updateBook);
 }
--- src/main/java/com/github/aakke/javaspringproject/service/BookServiceImpl.java
+++ src/main/java/com/github/aakke/javaspringproject/service/BookServiceImpl.java
@@ -1,5 +1,58 @@
 package com.github.aakke.javaspringproject.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import com.github.aakke.javaspringproject.model.Book;
+import com.github.aakke.javaspringproject.repository.BookRepository;
+
+@Service
 public class BookServiceImpl implements BookService{
 
+	private final BookRepository bookRepo;
+	
+	public BookServiceImpl(final BookRepository repo) {
+		this.bookRepo = repo;
+	}
+
+	@Override
+	public Book findById(long id) {
+		return bookRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("No book found with id: " + id));
+	}
+
+	@Override
+	public List<Book> findAll() {
+		var allBooks = bookRepo.findAll();
+		final List<Book> booksList = new ArrayList<>();
+		allBooks.forEach(booksList::add);
+		return booksList;
+	}
+
+	@Override
+	public void deleteById(long id) {
+		bookRepo.deleteById(id);
+	}
+
+	@Override
+	public Book create(Book newBook) {
+		return bookRepo.save(newBook);
+	}
+
+	@Override
+	public Book update(long id, Book updateBook) {
+		final Optional<Book> existingBook = bookRepo.findById(id);
+		
+		if (existingBook.isPresent()) {
+			return bookRepo.save(updateBook);
+		} 
+		else {
+			throw new EntityNotFoundException("No book found with id: " + id);
+		}
+	}
+	
 }
