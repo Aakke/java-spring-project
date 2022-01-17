9fd9c9d29fdce6edc6462e22ec518be939af1507
Aleksi Tani <aleksitani@gmail.com>
2022-01-17 19:30:53 +0200

    Add missing delete methods from Author and reader controllers.

--- src/main/java/com/github/aakke/javaspringproject/controller/AuthorController.java
+++ src/main/java/com/github/aakke/javaspringproject/controller/AuthorController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,10 @@ public class AuthorController {
 	public ResponseEntity<Author> updateAuthor(@PathVariable final int id, @RequestBody final Author author) {
 		Author updatedAuthor = authorService.update(id, author);
 		return ResponseEntity.ok().body(updatedAuthor);
+	}
+	
+	@DeleteMapping("/author/{id}")
+	public void deleteAuthorById(@PathVariable final int id) {
+		authorService.deleteById(id);
 	}
 }
--- src/main/java/com/github/aakke/javaspringproject/controller/ReaderController.java
+++ src/main/java/com/github/aakke/javaspringproject/controller/ReaderController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,10 @@ public class ReaderController {
 		// TODO: eTag/If-match headers.
 		Reader updatedReader = readerService.update(id, reader);
 		return ResponseEntity.ok().body(updatedReader);
+	}
+	
+	@DeleteMapping("/reader/{id}")
+	public void deleteReaderById(@PathVariable final int id) {
+		readerService.deleteById(id);
 	}
 }
